1f27b25b32ab99072b0c793cd165f40a781ac2c3
matheusferro <matheussouzaferro@gmail.com>
2021-03-01 21:48:03 -0300

    CONDICIONAIS, LOOPS E CLASSE

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,19 @@
 fun main(args: Array<String>) {
+    var contaJose = Conta()
+    contaJose.titular = "José"
+    var contaJoao = Conta()
+    mostrarConta(contaJoao)
+    mostrarConta(contaJose)
+}
+
+class Conta(){
+    var titular: String = "João"
+    var numero: String = "11120222"
+    var saldo: Double = 0.0
+
+}
+
+fun mostrarConta(conta: Conta){
     /**
      * Toda variável deve ser iniciada.
      * Variáveis Imutáveis          = val
@@ -10,10 +25,10 @@ fun main(args: Array<String>) {
      *
      * 1-FOR:
      */
+    val titular: String = conta.titular
+    val numeroConta: String = conta.numero
+    var saldo: Double = conta.saldo
     for (i in -1..3) {                                  //RANGES. POSSIVEL USAR O "downTo" , step, until,
-        val titular: String = "José"
-        val numeroConta: String = "0020.01010.00-1"
-        var saldo: Double = 1000.0
         saldo *= i
 
         //String Template
