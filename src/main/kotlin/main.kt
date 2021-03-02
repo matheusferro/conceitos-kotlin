c8e093977ebfa6d02ad869c1a85a50bd58252d90
matheusferro <matheussouzaferro@gmail.com>
2021-03-01 22:20:08 -0300

    adicionando getter e setters

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,67 @@
 fun main(args: Array<String>) {
     var contaJose = Conta()
     contaJose.titular = "José"
+    contaJose.deposito(100.0)
+    contaJose.saque(10.0)
+    println(contaJose.saldo)
+    //mostrarConta(contaJose)
+
     var contaJoao = Conta()
-    mostrarConta(contaJoao)
-    mostrarConta(contaJose)
+    contaJoao.deposito(20.0)
+    contaJoao.deposito(5.0)
+    println(contaJoao.saldo)
+    //mostrarConta(contaJoao)
+
+    if (contaJose.transfere(90.0, contaJoao)) {
+        println("Transferência bem sucedida.")
+    } else {
+        println("Erro na transferência.")
+    }
+
+    println("Saldo ${contaJose.titular}: ${contaJose.saldo}")
+    println("Saldo ${contaJoao.titular}: ${contaJoao.saldo}")
 }
 
-class Conta(){
+class Conta() {
     var titular: String = "João"
     var numero: String = "11120222"
     var saldo: Double = 0.0
+        private set
 
+    /**
+     * GETTERS E SETTERS PODEM SER UTILIZADOS COMO PROPERTUIES
+     *
+     *  EX:
+     *      var teste = "teste"
+     *          set(value: String){
+     *              if(value){
+     *                  field = value
+     *              }
+     *          }
+     *          private get
+     */
+
+    fun deposito(valor: Double) {
+        this.saldo += valor
+    }
+
+    fun saque(valor: Double) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+            destino.deposito(valor)
+            return true
+        }
+        return false
+    }
 }
 
-fun mostrarConta(conta: Conta){
+fun mostrarConta(conta: Conta) {
     /**
      * Toda variável deve ser iniciada.
      * Variáveis Imutáveis          = val
@@ -28,7 +76,7 @@ fun mostrarConta(conta: Conta){
     val titular: String = conta.titular
     val numeroConta: String = conta.numero
     var saldo: Double = conta.saldo
-    for (i in -1..3) {                                  //RANGES. POSSIVEL USAR O "downTo" , step, until,
+    for (i in -1..3) {//RANGES. POSSIVEL USAR O "downTo" , step, until,
         saldo *= i
 
         //String Template
@@ -68,11 +116,11 @@ fun avaliaSaldo(saldo: Double) {
      * IF tradicional
 
     if(saldo < 0) {
-        println("Saldo negativo: $saldo")
+    println("Saldo negativo: $saldo")
     }else if(saldo == 0.0){
-        println("Saldo neutro: $saldo")
+    println("Saldo neutro: $saldo")
     }else{
-        println("Saldo positivo: $saldo")
+    println("Saldo positivo: $saldo")
     }
 
      */
