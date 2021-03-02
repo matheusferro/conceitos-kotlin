abcf87f1b3bf101e0ead9b6a4115fcea024d8b4d
matheusferro <matheussouzaferro@gmail.com>
2021-03-02 17:39:07 -0300

    POLIMORFISMO

---
+++ src/main/kotlin/CalculadoraBonificacao.kt
@@ -0,0 +1,11 @@
+class CalculadoraBonificacao() {
+    var total: Double = 0.0
+        private set
+
+    /**
+     * Aplicando conceito de polimorfismo.
+     */
+    fun registra(funcionario: Funcionario){
+        this.total += funcionario.bonificacao
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,42 +1,6 @@
 fun main(args: Array<String>) {
     println("Bem vindo ao bytebank")
     //gerenciarConta()
-
-    val funcionario = Funcionario(
-        nome = "Alex",
-        cpf = "111.111.111-11",
-        salario = 1000.0,
-    )
-
-    println("nome: ${funcionario.nome}")
-    println("cpf: ${funcionario.cpf}")
-    println("salário: ${funcionario.salario}")
-    println("bonificação: ${funcionario.bonificacao}")
-    println("-----------------------------------------")
-
-    val gerente = Gerente(
-        nome = "Jorge",
-        cpf = "222.222.222-22",
-        salario = 2000.0,
-        senha = "1234"
-    )
-    println("nome: ${gerente.nome}")
-    println("cpf: ${gerente.cpf}")
-    println("salário: ${gerente.salario}")
-    println("bonificação: ${gerente.bonificacao}")
-    println("-----------------------------------------")
-
-    val diretor = Diretor(
-        nome = "Jorge",
-        cpf = "222.222.222-22",
-        salario = 2000.0,
-        senha = "1234",
-        plr = 100.0
-    )
-    println("nome: ${diretor.nome}")
-    println("cpf: ${diretor.cpf}")
-    println("salário: ${diretor.salario}")
-    println("bonificação: ${diretor.bonificacao}")
+    //testaFuncionarios()
 }
-
 
