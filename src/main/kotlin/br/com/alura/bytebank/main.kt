f00609e4cac7d95f1135b3bdf2651cf0220282be
matheusferro <matheussouzaferro@gmail.com>
2021-03-03 15:57:44 -0300

    EXEMPLO DE OBJECT EXPRESSION

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -7,10 +7,33 @@ fun main() {
     testaFuncionarios()
     testaContaDiferente()
     testaAutenticacao()
-
+    //objectExpression()
     println("Temos ${Conta.totalContas} contas.")
     val palavra: String = ""
     //Utilizando String da biblioteca java
     val palavraJava: java.lang.String = java.lang.String("")//nome completo
     val palavraJava2: StringJava = StringJava("")// utilizando apelido para o pacote, definido com o 'as'
+}
+
+fun objectExpression() {
+    /**
+     * Object expressions:
+     * normalmente usado para android na ação de um botão:
+     *
+     * window.addMouseListener(object : MouseAdapter() {
+     *      override fun mouseClicked(e: MouseEvent) { /*...*/ }
+     *      override fun mouseEntered(e: MouseEvent) { /*...*/ }
+     * })
+     *
+     */
+    var objCliente = object {
+        val nome: String = "clienteObj"
+        val cpf: String = "112.221.331-00"
+        val senha: String = "senhaBoaESegura"
+
+        fun autentica(senha: String) = this.senha == senha
+    }
+    println("nome: ${objCliente.nome}")
+    println("cpf: ${objCliente.cpf}")
+    println(objCliente.autentica("senha123"))
 }
