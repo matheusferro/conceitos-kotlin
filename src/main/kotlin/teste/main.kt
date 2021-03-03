e799a5fbcaa6efda43dd5b57d40bfad51bba5ef1
matheusferro <matheussouzaferro@gmail.com>
2021-03-03 14:43:22 -0300

    import e alias

--- src/main/kotlin/teste/main.kt
+++ src/main/kotlin/teste/main.kt
@@ -1,9 +1,14 @@
+import java.lang.String as StringJava
+
 fun main() {
-    println("Bem vindo ao bytebank")
-    gerenciarConta()
-    testaFuncionarios()
-    testaContaDiferente()
-    testaAutenticacao()
+//    println("Bem vindo ao bytebank")
+//    gerenciarConta()
+//    testaFuncionarios()
+//    testaContaDiferente()
+//    testaAutenticacao()
+
+    val palavra: String = ""
+    //Utilizando String da biblioteca java
+    val palavraJava: java.lang.String = java.lang.String("")//nome completo
+    val palavraJava2: StringJava = StringJava("")// utilizando apelido para o pacote, definido com o 'as'
 }
-
-
