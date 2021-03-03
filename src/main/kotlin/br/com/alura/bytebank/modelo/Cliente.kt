4cf58cb0a57e23f33722b9d6ff94a6c4c2580fdf
matheusferro <matheussouzaferro@gmail.com>
2021-03-03 15:19:23 -0300

    ADICIONADO ENDERECO AO CLIENTE

--- src/main/kotlin/br/com/alura/bytebank/modelo/Cliente.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/Cliente.kt
@@ -3,7 +3,8 @@ package br.com.alura.bytebank.modelo
 class Cliente(
     val nome:  String,
     val cpf: String,
-    val senha: String
+    val senha: String,
+    val endereco: Endereco = Endereco()
 ) : Autenticavel {
     override fun autentica(senha: String): Boolean{
         return this.senha == senha
---
+++ src/main/kotlin/br/com/alura/bytebank/modelo/Endereco.kt
@@ -0,0 +1,13 @@
+package br.com.alura.bytebank.modelo
+
+class Endereco(
+    val logradouro: String = "",
+    val numero: String = "",
+    val bairro: String = "",
+    val cidade: String = "",
+    val estado: String = "",
+    val cep: String = "",
+    val complemento: String = ""
+) {
+
+}
