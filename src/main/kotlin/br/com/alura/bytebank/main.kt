248a2c833014b7dd52c1e89941c9317b579b7d17
matheusferro <matheussouzaferro@gmail.com>
2021-03-04 12:03:25 -0300

    ANY, toString(), hashCode() e equals()

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -14,26 +14,3 @@ fun main() {
     val palavraJava: java.lang.String = java.lang.String("")//nome completo
     val palavraJava2: StringJava = StringJava("")// utilizando apelido para o pacote, definido com o 'as'
 }
-
-fun objectExpression() {
-    /**
-     * Object expressions:
-     * normalmente usado para android na ação de um botão:
-     *
-     * window.addMouseListener(object : MouseAdapter() {
-     *      override fun mouseClicked(e: MouseEvent) { /*...*/ }
-     *      override fun mouseEntered(e: MouseEvent) { /*...*/ }
-     * })
-     *
-     */
-    var objCliente = object {
-        val nome: String = "clienteObj"
-        val cpf: String = "112.221.331-00"
-        val senha: String = "senhaBoaESegura"
-
-        fun autentica(senha: String) = this.senha == senha
-    }
-    println("nome: ${objCliente.nome}")
-    println("cpf: ${objCliente.cpf}")
-    println(objCliente.autentica("senha123"))
-}
---
+++ src/main/kotlin/br/com/alura/bytebank/teste/testeAny.kt
@@ -0,0 +1,40 @@
+package br.com.alura.bytebank.teste
+
+import br.com.alura.bytebank.modelo.Endereco
+
+fun main(){
+    var endereco: Endereco = Endereco(
+        "Rua do meio",
+        "157",
+        "Centro",
+        "Sp",
+        "Sp",
+        "000000",
+        "nenhum"
+    )
+    var endereco2 = Endereco(
+        "Rua do meio",
+        "157",
+        "Centro",
+        "Sp",
+        "Sp",
+        "000000",
+        "nenhum"
+    )
+    println(endereco.hashCode())
+    println(endereco2.hashCode())
+    println(endereco.equals(endereco2))
+
+    imprime(endereco)
+    imprime("Teste")
+    imprime(1601.0)
+
+}
+
+/**
+ * Utilizando Objeto ANY
+ */
+fun imprime(valor: Any):Any{
+    println(valor)
+    return valor;
+}
