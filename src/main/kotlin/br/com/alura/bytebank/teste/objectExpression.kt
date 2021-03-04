db483176f5e8a8e0a0b00b76d76761cea2a6dd51
matheusferro <matheussouzaferro@gmail.com>
2021-03-04 12:00:41 -0300

    Objectexpression

---
+++ src/main/kotlin/br/com/alura/bytebank/teste/objectExpression.kt
@@ -0,0 +1,24 @@
+package br.com.alura.bytebank.teste
+
+fun objectExpression() {
+    /**
+     * Object expressions:
+     * normalmente usado para android na ação de um botão:
+     *
+     * window.addMouseListener(object : MouseAdapter() {
+     *      override fun mouseClicked(e: MouseEvent) { /*...*/ }
+     *      override fun mouseEntered(e: MouseEvent) { /*...*/ }
+     * })
+     *
+     */
+    var objCliente = object {
+        val nome: String = "clienteObj"
+        val cpf: String = "112.221.331-00"
+        val senha: String = "senhaBoaESegura"
+
+        fun autentica(senha: String) = this.senha == senha
+    }
+    println("nome: ${objCliente.nome}")
+    println("cpf: ${objCliente.cpf}")
+    println(objCliente.autentica("senha123"))
+}
