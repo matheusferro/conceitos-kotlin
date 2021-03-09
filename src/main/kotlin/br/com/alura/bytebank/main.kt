fc4f9a4b23eeb7bffeeec2da46a7becab689aa80
matheusferro <matheussouzaferro@gmail.com>
2021-03-09 11:35:24 -0300

    NAO UTILIZACAO DE UM PARAMETRO

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,8 +1,8 @@
 fun main() {
 
-    val funcaoLambda: (Int, Int) -> Int = { a, b ->
+    val funcaoLambda: (Int, Int) -> Int = { _, b ->
         println("Executando lambda")
-        a + b //em uma função lambda a ultima instrução determina o retorno.
+        b + 10 //em uma função lambda a ultima instrução determina o retorno.
     }
 
     println(funcaoLambda(20, 7))
