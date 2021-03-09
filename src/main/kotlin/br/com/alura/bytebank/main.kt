65dcbbd4ae6a904d620c0c7589865e2d422bb566
matheusferro <matheussouzaferro@gmail.com>
2021-03-09 11:57:46 -0300

    MULTIPLOS RETORNOS FUNCAO LAMBDA

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -17,4 +17,24 @@ fun main() {
         return a + b //necessário colocar o return
     }
     println(funcaoAnonima(201,783))
+
+    /**
+     * Multiplos retornos em uma função lambda.
+     */
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
+        //Quando temos somente um parâmetro, podemos acessar ele com: it
+        //if(it > 2000.0){
+        if(salario > 2000.0){
+            /**
+             * Não podemos utlizar somente return it + 500.0
+             * pois retornaria para nossa função main diretamente
+             * então utilizamos o recurso de "returns and jumps"
+             * https://kotlinlang.org/docs/returns.html
+             */
+            return@lambda salario + 100.0//
+        }
+        //Aqui é opcional
+        return@lambda salario + 500.0
+    }
+    println(calculaBonificacao(3000.0))
 }
