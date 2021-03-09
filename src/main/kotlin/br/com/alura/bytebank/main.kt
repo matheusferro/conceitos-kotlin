4b0ffe5e8a04b3c94db3e1dd0f90ca3cc868ada4
matheusferro <matheussouzaferro@gmail.com>
2021-03-09 12:16:26 -0300

    INTRODUCAO A HIGH-ORDER FUNCTION

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,5 +1,35 @@
+import br.com.alura.bytebank.modelo.Endereco
+
 fun main() {
 
+    val testeFuncao: () -> Unit
+    //let -> Utiliza generics
+    //Objeto Endereco
+    Endereco().let{
+        it
+    }
+
+    //String
+    "String qualquer".let{
+        it
+    }
+
+    //Int
+    177.let{
+        it
+    }
+
+    //Por ter uma função como parâmetro, podemos utilizar direto.
+    teste{ }
+    //teste(1){}    quando temos dois parametros, utlimo sendo uma função
+    //teste({},1)   quando temos dois parametros, primeiro sendo uma função
 }
 
 
+/**
+ * High-Order Function -> Uma função que recebe/devolve outra...
+ */
+
+fun teste(bloco: () -> Unit){
+
+}
