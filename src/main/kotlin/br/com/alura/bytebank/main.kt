8f9e922c736819dd82ea7280e9b6c3de304ab494
matheusferro <matheussouzaferro@gmail.com>
2021-03-09 11:36:38 -0300

    INFERINDO NO TIPO

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,8 +1,8 @@
 fun main() {
 
-    val funcaoLambda: (Int, Int) -> Int = { _, b ->
+    val funcaoLambda = { a: Int, b: Int ->
         println("Executando lambda")
-        b + 10 //em uma função lambda a ultima instrução determina o retorno.
+        a + b //em uma função lambda a ultima instrução determina o retorno.
     }
 
     println(funcaoLambda(20, 7))
