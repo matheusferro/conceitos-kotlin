ce7269be83c70e297ceb3d3222c1c258bf20854a
matheusferro <matheussouzaferro@gmail.com>
2021-03-09 11:40:10 -0300

    ADICIONANDO PARAMETRO FUNCAO ANONIMA

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -7,8 +7,14 @@ fun main() {
 
     println(funcaoLambda(20, 7))
 
-    val funcaoAnonima: () -> Unit = fun (){
+    /**
+     * Consideramos a função anonima, quando queremos deixar explicito
+     * o que queremos colocar como parâmetro, retorno e o escopo de execução
+     * do código.
+     */
+    val funcaoAnonima: (Int, Int) -> Int = fun (a, b): Int{
         println("Executando função anonima")
+        return a + b //necessário colocar o return
     }
-    println(funcaoAnonima())
+    println(funcaoAnonima(201,783))
 }
