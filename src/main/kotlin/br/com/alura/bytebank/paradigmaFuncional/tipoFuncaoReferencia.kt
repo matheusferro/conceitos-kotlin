b20cce31f00850dc5cad750264fa491ae20f6da3
matheusferro <matheussouzaferro@gmail.com>
2021-03-09 11:30:19 -0300

    ADICIONANDO PARAMETROS TIPOFUNCAO

--- src/main/kotlin/br/com/alura/bytebank/paradigmaFuncional/tipoFuncaoReferencia.kt
+++ src/main/kotlin/br/com/alura/bytebank/paradigmaFuncional/tipoFuncaoReferencia.kt
@@ -1,16 +1,33 @@
 package br.com.alura.bytebank.paradigmaFuncional
 
-private fun testaTipoFuncaoReferencia() {
+fun main(){
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
+}
+
+fun testaTipoFuncaoReferencia() {
     /**
      * Tipo função:                  () -> Unit
      * Indica Referência da função:  ::
+     *
+     * Alterado assinatura do método
      */
-    val minhaFuncao: () -> Unit = ::teste
+    val minhaFuncao: (Int, Int) -> Int = ::soma
     //Lazy Evaluation -> Quando a função não é executada quando é inicializada ou indica qual é o seu retorno
     println(minhaFuncao)
-    //Agora sim, estamos executando a função: ()
-    println(minhaFuncao())
+    /**
+     * Agora sim, estamos executando a função: ()
+     *
+     * Passando os parâmetros obrigatórios.
+     */
+    println(minhaFuncao(900,600))
+}
 
+fun soma(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun testaTipoFuncaoClasse() {
     /**
      * Podemos fazer a mesma implementação a partir de uma classe
      *
@@ -18,21 +35,23 @@ private fun testaTipoFuncaoReferencia() {
      * ao executar.
      *
      * Aqui não usamos a referência:        ::      <- Pois não estamos nos tratando de métodos.
+     *
+     * Alterando a assinatura
      */
-    val minhaFuncaoClasse: () -> Unit = Teste()
-    println(minhaFuncaoClasse())
-}
-
-fun teste() {
-    println("Função executada!")
+    val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
+    println(minhaFuncaoClasse(1,2))
 }
 
 /**
  * Nosso tipo função é como se fosse uma interface,
  * a apartir do que foi definido ((a:Int,b:Int)->Unit) é criado o invoke
+ *
+ * Adicionando parametros
  */
-class Teste : () -> Unit {
-    override fun invoke() {
-        println("Utilizando classe")
+class Soma : (Int, Int) -> Int {
+
+    //Podemos usar: override fun invoke(p1: Int, p2: Int): Int = p1 + p2
+    override fun invoke(p1: Int, p2: Int): Int {
+        return p1 + p2
     }
 }
