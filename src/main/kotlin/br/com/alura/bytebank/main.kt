c9c100954afdc6c4e78428e1295723a14ce8aa69
matheusferro <matheussouzaferro@gmail.com>
2021-03-09 12:37:02 -0300

    HIGH-ORDER FUNCTIONS

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,35 +1,41 @@
 import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.SistemaInterno
+import br.com.alura.bytebank.modelo.interfaces.Autenticavel
 
 fun main() {
 
     val testeFuncao: () -> Unit
     //let -> Utiliza generics
     //Objeto Endereco
-    Endereco().let{
-        it
-    }
+    //val endereco: Endereco = Endereco(logradouro = "Rua do meio", numero = "465")
+    //val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    //println(enderecoEmMaiusculo)
 
-    //String
-    "String qualquer".let{
-        it
-    }
+    val enderecoEmMaiusculo = Endereco(logradouro = "Rua do meio", numero = "465")
+        .let { endereco ->
+            "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+        }.let(::println)
 
-    //Int
-    177.let{
-        it
-    }
+    listOf(
+        Endereco(complemento = "Bloco 07"),
+        Endereco(),
+        Endereco(complemento = "Casa")
+    ).filter { endereco -> endereco.complemento.isNotEmpty() }
+        .let(::println)
 
-    //Por ter uma função como parâmetro, podemos utilizar direto.
-    teste{ }
-    //teste(1){}    quando temos dois parametros, utlimo sendo uma função
-    //teste({},1)   quando temos dois parametros, primeiro sendo uma função
+    soma(107, 2, resultado = (::println))
+
+    val autenticavel = object : Autenticavel{
+        val senha: String = "senhaSegura"
+        override fun autentica(senha: String): Boolean = this.senha == senha
+    }
+    SistemaInterno().entra(autenticavel, "senhaSegura", autenticado ={
+        println("Log-in realizado pela high-order function")
+    })
 }
 
-
-/**
- * High-Order Function -> Uma função que recebe/devolve outra...
- */
-
-fun teste(bloco: () -> Unit){
-
+fun soma(v1: Int, v2: Int, resultado: (Int) -> Unit) {
+    println("Iniciando a soma")
+    resultado(v1 + v2)
+    println("Fechando a soma")
 }
--- src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -4,9 +4,10 @@ import br.com.alura.bytebank.modelo.interfaces.Autenticavel
 
 class SistemaInterno {
 
-    fun entra(admin: Autenticavel, senha: String){
+    fun entra(admin: Autenticavel, senha: String, autenticado: () -> Unit = {}){
         if(admin.autentica(senha)){
             println("Bem vindo ao bytebank")
+            autenticado()
         }else{
             println("Falha ao logar")
         }
